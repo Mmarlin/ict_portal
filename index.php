00d9e7f2a6d5605f6b79a458bb63afac74f225ab
Marlin Shah <marlinshah124@gmail.com>
2021-06-02 21:01:17 +0530

    adding new index file

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+    //session_destroy();
+    echo "<script>window.location.href='https://www.ictmu.in/ict_portal/ictlogin/login';</script>";
+?>
+
+<html> 
+<head>
+    <script data-ad-client="ca-pub-6434956637082151" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252"> 
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <title>ICT Portal</title> 
+    <link rel = "icon" href ="/ict_portal/ictlogin/logo/Marwadi_University_logo.png" type = "image/ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <style>
+      .button {
+  background-color: #008CBA;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 20px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+  </style>
+</head> 
+
+<body> 
+
+<br/>
+<br/>
+<br/>
+<br/>
+
+<br>
+<center>
+<h2> Welcome To ICT Portal </h2> 
+
+<h2> Thank you for your convenience </h2> 
+<h2> Click on the Blue button to go to Portal Login</h2>
+<div class="w3-content w3-section" style="max-width:500px">
+  <img src="ict_portal/ictlogin/logo/FB_IMG_1571335792415.jpg" style="width:100%">
+</div>
+
+<h5>click here to redirect Portal </h5>
+<a href="https://www.ictmu.in/ict_portal/ictlogin/login" class="button" type="button">ICT Portal</a>
+</center>
+
+
+  </body>
+  
+</html> 
